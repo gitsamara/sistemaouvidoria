b25c034b32ed76d2916f3dd1308a2b08eef22166
Samara Sales <contato.samarasales@gmail.com>
2023-10-04 11:22:56 -0300

    adicionando main e atualizando versao 02

---
+++ main.py
@@ -0,0 +1,35 @@
+from ouvidoria_final import *
+
+def main():
+    conn = abrirBancoDados("localhost", "root", "s@M{0511_", "ouvidoria")
+
+    while True:
+        print("OUVIDORIA")
+        print("Estamos aqui para lhe ajudar! Registre sua manifestação e nós a levaremos ao conhecimento da instância pertinente.")
+
+        print("1. Listar Manifestações")
+        print("2. Adicionar Nova Manifestação")
+        print("3. Alterar Manifestação")
+        print("4. Excluir Manifestação")
+        print("5. Sair do Sistema")
+
+        opcao = int(input("Digite a sua opção: "))
+
+        if opcao == 1:
+            listar_manifestacoes(conn)
+        elif opcao == 2:
+            adicionar_manifestacao(conn)
+        elif opcao == 3:
+            alterar_manifestacao(conn)
+        elif opcao == 4:
+            excluir_manifestacao(conn)
+        elif opcao == 5:
+            break
+        else:
+            print("Opção inválida! Tente outra vez.")
+
+    encerrarBancoDados(conn)
+    print("Obrigado por colaborar com nosso sistema! Até mais.")
+
+if __name__ == "__main__":
+    main()
--- ouvidoria_versao02.py
+++ ouvidoria_versao02.py
@@ -1,7 +1,7 @@
 from operacoesbd import *
 
 def listar_manifestacoes(conn):
-    consulta_sql = "SELECT * FROM ouvidoria_bd"
+    consulta_sql = "select * from ouvidoria_bd"
     manifestacoes = listarBancoDados(conn, consulta_sql)
 
     if not manifestacoes:
@@ -22,7 +22,7 @@ def adicionar_manifestacao(conn):
     assunto_manifestacao = input("Digite o assunto: ")
     texto_manifestacao = input("Fale aqui: ")
 
-    consulta_manifestacao = "INSERT INTO ouvidoria_bd (tipo_manifestacao, assunto, manifestacao) VALUES (%s, %s, %s)"
+    consulta_manifestacao = "insert into ouvidoria_bd (tipo_manifestacao, assunto, manifestacao) values (%s, %s, %s)"
     dados = [tipo_manifestacao, assunto_manifestacao, texto_manifestacao]
 
     insertNoBancoDados(conn, consulta_manifestacao, dados)
@@ -32,12 +32,12 @@ def alterar_manifestacao(conn):
     listar_manifestacoes(conn)
     codigo_manifestacao = int(input("Digite o código da manifestação que deseja alterar: "))
 
-    consulta_sql = "SELECT * FROM ouvidoria_bd"
+    consulta_sql = "select * from ouvidoria_bd"
     manifestacoes = listarBancoDados(conn, consulta_sql)
 
     if codigo_manifestacao in range(1, len(manifestacoes) + 1):
         novo_texto = input("Digite seu novo texto: ")
-        atualizar_manifestacao = "UPDATE ouvidoria_bd SET manifestacao = %s WHERE codigo = %s"
+        atualizar_manifestacao = "update ouvidoria_bd set manifestacao = %s where codigo = %s"
         dados_atualizacao = (novo_texto, manifestacoes[codigo_manifestacao - 1][0])
         atualizarBancoDados(conn, atualizar_manifestacao, dados_atualizacao)
         print("Manifestação alterada com sucesso!")
@@ -48,15 +48,16 @@ def excluir_manifestacao(conn):
     listar_manifestacoes(conn)
     codigo_manifestacao = int(input("Digite o código da manifestação que deseja excluir: "))
 
-    consulta_sql = "SELECT * FROM ouvidoria_bd"
+    consulta_sql = "select * from ouvidoria_bd"
     manifestacoes = listarBancoDados(conn, consulta_sql)
 
     if codigo_manifestacao in range(1, len(manifestacoes) + 1):
         id_manifestacao_excluir = manifestacoes[codigo_manifestacao - 1][0]
-        excluir_manifestacao = "DELETE FROM ouvidoria_bd WHERE codigo = %s"
+        excluir_manifestacao = "delete from ouvidoria_bd where codigo = %s"
         dados_exclusao = (id_manifestacao_excluir,)
         excluirBancoDados(conn, excluir_manifestacao, dados_exclusao)
         manifestacoes.pop(codigo_manifestacao - 1)
         print("Manifestação excluída!")
     else:
         print("Número de manifestação inválido! Tente novamente.")
+        
